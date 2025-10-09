cbda12542a3ef1dab5dfb72b894352b6b5294aac
NatanPantoja <natanaelletras@gmail.com>
2025-10-09 10:11:24 -0300

    router que faltou exportar

--- src/router.js
+++ src/router.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 
+// controller
 import { CreateClientController } from "./controller/CreateClientController.js";
 
+// Importante criar uma instância do Router
 const router = Router();
 
+// Rota para criar um usuário
 router.post("/user", new CreateClientController().handle);
 
 export default router; // Não se esqueça de exportar o router!
